ed9d79b88f7eebfb3c688519f3e3fc4530876b91
Tinisha <tinishadavis159@gmail.com>
2025-02-18 15:15:19 -0800

    Updated Item.java and test code

--- src/main/java/utils/Item.java
+++ src/main/java/utils/Item.java
@@ -7,7 +7,14 @@ package utils;
  * Therefore, we use double for weight and value.
  */
 public class Item {
+  /**
+   * Weight of the item
+   */
   private final int weight;
+
+  /**
+   * Value of the item
+   */
   private final int value;
 
   /**
@@ -18,15 +25,40 @@ public class Item {
     this.value = value;
   }
 
+  /**
+   * Retrieves items weight
+   *
+   * @return items weight
+   */
   public int getWeight() {
     return weight;
   }
 
+  /**
+   * Retrieves item's value
+   *
+   * @return item value
+   */
   public int getValue() {
     return value;
   }
 
+  /**
+   * Retrieves the ratio of the item
+   *
+   * @return value / weight
+   */
   public double getRatio() {
     return (double) value / weight;
+  }
+
+  /**
+   * Presents this item's information in a nice and neat way
+   *
+   * @return A neat display of this item's information
+   */
+  @Override
+  public String toString() {
+    return value + "\n" + weight;
   }
 }
